f135a3ba8b1d691a5c86e02f61932a7e289ba726
Myron <mshen32@wisc.edu>
2021-04-20 16:31:39 -0500

    Create LC_429.py

---
+++ DailyChallenge/LC_429.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root: return []
+        queue = deque([root])
+        res = []
+        
+        while queue:
+            N = len(queue)
+            tmp = []
+            while N:
+                node = queue.popleft()
+                for child in node.children:
+                    queue.append(child)
+                tmp.append(node.val)
+                N -= 1
+            res.append(tmp)
+            
+        return res
+            
