df57a1912f162bbfd85af56a2b51b7ce9356854e
Myron <mshen32@wisc.edu>
2021-04-20 16:21:57 -0500

    Create LC_590.py

---
+++ DailyChallenge/LC_590.py
@@ -0,0 +1,40 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        """
+        Iterative
+        """
+        if not root: return []
+        res = []
+        queue = deque([root])
+        res_queue = deque([])
+        while queue:
+            node = queue.popleft()
+            for child in node.children:
+                queue.appendleft(child)
+            res_queue.appendleft(node.val)
+        return res_queue
+        
+        """
+        recursive
+        """
+        
+        """
+        if not root: return []
+        res = []
+        
+        def DFS(node):
+            for child in node.children:
+                DFS(child)
+            res.append(node.val)
+            
+        DFS(root)
+        return res
+        """
