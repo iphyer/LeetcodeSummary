9f72c2293156bd0daf8e7ff88b91b5d6f677f522
Myron <mshen32@wisc.edu>
2021-04-27 15:17:40 -0500

    Update LC_326.py

--- DailyChallenge/LC_326.py
+++ DailyChallenge/LC_326.py
@@ -8,3 +8,9 @@ class Solution:
                 res *= 3
         return False
         
+# Another Solution
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        # 1162261467 = 3^19 < 3^⌊19.56⌋ = 2^31
+        return n > 0 and 1162261467 % n == 0
