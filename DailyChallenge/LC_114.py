7c3d5be3521f959cf2a554adbca3308a0d8692a8
Myron <mshen32@wisc.edu>
2021-05-15 00:12:21 -0500

    Create LC_114.py

---
+++ DailyChallenge/LC_114.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        # edge cases
+        if not root: return []
+        # general cases
+        curr = root
+        while curr:
+            if curr.left:
+                right_p = curr.left
+                while right_p.right:
+                    right_p = right_p.right
+                right_p.right = curr.right
+                curr.right = curr.left
+                curr.left = None
+            curr = curr.right
