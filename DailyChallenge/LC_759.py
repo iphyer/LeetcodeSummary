9c7674308a3d226ea38c27b0acb0bb243feec842
Myron <mshen32@wisc.edu>
2021-06-17 06:44:22 -0500

    Create LC_759.py

---
+++ DailyChallenge/LC_759.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
+        # pick each ele to be maximum ele
+        # Then sliding windows
+        res = 0
+        for ind,num in enumerate(nums):
+            # num itself is in or not
+            if left <= num <= right:
+                res += 1
+                # sliding windows
+                # p1, p2 in 
+                #   in range 0~len(nums)
+                #   nums(p1) < num
+                #   nums(p2) < num
+                p1, p2 = ind-1, ind+1
+                while p1 > -1 and nums[p1] < num:
+                    p1 -= 1
+                    res += 1
+
+                while p2 < len(nums) and nums[p2] <= num:
+                    p2 += 1
+                    res += 1
+
+                if p1+1 != ind and p2-1 != ind:
+                    res += (ind-p1-1) * (p2-ind-1)
+        return res
+                
+            
