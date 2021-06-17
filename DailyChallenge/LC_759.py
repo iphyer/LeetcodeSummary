9065bd8daded17e2470f6228c746041d303c1d9c
Myron <mshen32@wisc.edu>
2021-06-17 06:56:59 -0500

    Update LC_759.py

--- DailyChallenge/LC_759.py
+++ DailyChallenge/LC_759.py
@@ -9,9 +9,9 @@ class Solution:
                 res += 1
                 # sliding windows
                 # p1, p2 in 
-                #   in range 0~len(nums)
+                #   in range 0~len(nums)-1
                 #   nums(p1) < num
-                #   nums(p2) < num
+                #   nums(p2) <= num
                 p1, p2 = ind-1, ind+1
                 while p1 > -1 and nums[p1] < num:
                     p1 -= 1
@@ -20,9 +20,7 @@ class Solution:
                 while p2 < len(nums) and nums[p2] <= num:
                     p2 += 1
                     res += 1
-
+                # Crossing num
                 if p1+1 != ind and p2-1 != ind:
                     res += (ind-p1-1) * (p2-ind-1)
         return res
-                
-            
