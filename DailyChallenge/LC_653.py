1462c78e9a7735ea8fb59c96b34389d0a174e750
Myron <mshen32@wisc.edu>
2021-08-24 19:29:59 -0500

    Create LC_653.py

---
+++ DailyChallenge/LC_653.py
@@ -0,0 +1,55 @@
+class Solution:
+    def findTarget(self, root: TreeNode, k: int) -> bool:
+        def pushLeft(st, root):
+            while root:
+                st.append(root)
+                root = root.left
+
+        def pushRight(st, root):
+            while root:
+                st.append(root)
+                root = root.right
+
+        def nextLeft(st):
+            node = st.pop()
+            pushLeft(st, node.right)
+            return node.val
+
+        def nextRight(st):
+            node = st.pop()
+            pushRight(st, node.left)
+            return node.val
+
+        stLeft, stRight = [], []
+        pushLeft(stLeft, root)
+        pushRight(stRight, root)
+
+        left, right = nextLeft(stLeft), nextRight(stRight)
+        while left < right:
+            if left + right == k: return True
+            if left + right < k:
+                left = nextLeft(stLeft)
+            else:
+                right = nextRight(stRight)
+        return False
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        val_list = []
+        queue = [root]
+        while queue:
+            node = queue.pop()
+            if k-node.val not in val_list:
+                val_list.append(node.val)
+            else:
+                return True
+            if node.left: queue.append(node.left)
+            if node.right: queue.append(node.right)
+        return False
