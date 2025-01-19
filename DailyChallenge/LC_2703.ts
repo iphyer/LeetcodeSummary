742f2a35145df5890387d7acd030160374e01488
Myron <shenmingren09@gmail.com>
2025-01-18 18:51:54 -0600

    Create LC_2703.ts

---
+++ DailyChallenge/LC_2703.ts
@@ -0,0 +1,12 @@
+#2703. Return Length of Arguments Passed
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+
+function argumentsLength(...args: JSONValue[]): number {
+    return args.length;
+    
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
